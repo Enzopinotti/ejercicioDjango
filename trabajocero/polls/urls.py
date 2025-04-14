702eb4f6d6863be624ef4f66b2a1825b9c7f5778
Enzo Pinotti <enzopinottii@gmail.com>
2025-04-14 19:12:09 -0300

    [Parte 3.1] Agrega vistas detail, results y vote, y sus rutas

--- trabajocero/polls/urls.py
+++ trabajocero/polls/urls.py
@@ -1,7 +1,11 @@
-# polls/urls.py
 from django.urls import path
-from . import views  
+from . import views
+
+app_name = "polls"  # esto es importante para usar {% url 'polls:detail' %}
 
 urlpatterns = [
-    path("", views.index, name="index"),  # 👈 Cuando entras a /polls/, va a la vista index
+    path("", views.index, name="index"),  # /polls/
+    path("<int:question_id>/", views.detail, name="detail"),  # /polls/34/
+    path("<int:question_id>/results/", views.results, name="results"),  # /polls/34/results/
+    path("<int:question_id>/vote/", views.vote, name="vote"),  # /polls/34/vote/
 ]
