7624d5f231cb093db26c43faeb1217b44c386d04
Enzo Pinotti <enzopinottii@gmail.com>
2025-04-14 19:47:04 -0300

    [Parte 4.1] Procesa formulario de votación y redirige a resultados

--- trabajocero/polls/views.py
+++ trabajocero/polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 
 def index(request):
@@ -15,4 +17,15 @@ def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No seleccionaste una opción.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
