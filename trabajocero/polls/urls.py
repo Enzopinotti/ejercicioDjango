230d00757c7b26d02603113712a21d7123e64223
Enzo Pinotti <enzopinottii@gmail.com>
2025-04-14 19:53:07 -0300

    [Mejora] Refactoriza vistas index, detail y results a vistas genéricas (CBV)

--- trabajocero/polls/urls.py
+++ trabajocero/polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "polls"  # esto es importante para usar {% url 'polls:detail' %}
-
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),  # /polls/
-    path("<int:question_id>/", views.detail, name="detail"),  # /polls/34/
-    path("<int:question_id>/results/", views.results, name="results"),  # /polls/34/results/
-    path("<int:question_id>/vote/", views.vote, name="vote"),  # /polls/34/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),  # queda como función
 ]
+
